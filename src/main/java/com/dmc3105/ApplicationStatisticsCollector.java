22b129f8ab752486b5d7a313d66c9836a3464de2
Говоров А. В <algovor04@gmail.com>
2024-01-17 01:46:15 +0700

    Add ApplicationStatisticsCollector class

---
+++ src/main/java/com/dmc3105/ApplicationStatisticsCollector.java
@@ -0,0 +1,52 @@
+package com.dmc3105;
+
+import com.dmc3105.statistics.collectors.EmptyStatisticsException;
+import com.dmc3105.statistics.collectors.StatisticsCollector;
+import com.dmc3105.statistics.types.Statistics;
+import com.dmc3105.typeidentifier.RegexTypeIdentifier;
+import com.dmc3105.typeidentifier.Type;
+import com.dmc3105.typeidentifier.TypeIdentifier;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class ApplicationStatisticsCollector
+{
+    private final SortedMap<Type, StatisticsCollector> typeStatisticsCollectorHashMap;
+    private final TypeIdentifier typeIdentifier = new RegexTypeIdentifier();
+
+    public ApplicationStatisticsCollector(StatisticsCollectorsFactory factory) {
+        typeStatisticsCollectorHashMap = new TreeMap<>();
+        typeStatisticsCollectorHashMap.put(Type.FLOAT, factory.createFloatStatisticsCollector());
+        typeStatisticsCollectorHashMap.put(Type.INTEGER, factory.createIntegerStatisticsCollector());
+        typeStatisticsCollectorHashMap.put(Type.STRING, factory.createStringStatisticsCollector());
+    }
+
+    public void collectStatistics(String value)
+    {
+        Type type = typeIdentifier.identify(value);
+        typeStatisticsCollectorHashMap.get(type).collectStatistics(value);
+    }
+
+    public void printStatistics()
+    {
+        for (var entry : typeStatisticsCollectorHashMap.entrySet()) {
+            System.out.println(entry.getKey());
+            StatisticsCollector collector = entry.getValue();
+            if (!collector.isEmpty())
+            {
+                Statistics statistics;
+                try {
+                    statistics = collector.getStatistics();
+                    System.out.println(statistics.asString());
+                } catch (EmptyStatisticsException e) {
+                    e.printStackTrace();
+                }
+            }
+            else
+            {
+                System.out.println("Статистика отсутствует");
+            }
+        }
+    }
+}
